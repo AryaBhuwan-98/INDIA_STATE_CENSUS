a4469d678939ddcfe288d7a02c60a4b4f3145205
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-02-28 13:14:35 +0530

    Add:Welcome to the indian state census

---
+++ src/main/java/com/bridgelabz/indianstatecensus/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.indianstatecensus;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the indian state census" );
+    }
+}
